41ee7ee4e7f5575f9d67c09ee6c21385dac6df62
Santiago Baliño <santiagobalino@hotmail.com>
2023-04-17 11:18:56 -0300

    Refactor styles without prettier

--- Logic JS Arbusta.js
+++ Logic JS Arbusta.js
@@ -19,9 +19,7 @@ const sortAndFillNumbers = (arr) => {
     const sortedArr = arr.sort((a, b) => b - a);
     const numbersBetween = [];
 
-    for (let i = sortedArr[0]; i > sortedArr[1]; i--) {
-        if (i === sortedArr[0]) continue;
-
+    for (let i = sortedArr[0] - 1; i > sortedArr[1]; i--) {
         numbersBetween.push(i);
     }
     return numbersBetween;
@@ -108,34 +106,7 @@ console.log(
  */
 
 const convertFirstLetterToUppercase = (str) => {
-    const alphabet = {
-        a: 'A',
-        b: 'B',
-        c: 'C',
-        d: 'D',
-        e: 'E',
-        f: 'F',
-        g: 'G',
-        h: 'H',
-        i: 'I',
-        j: 'J',
-        k: 'K',
-        l: 'L',
-        m: 'M',
-        n: 'N',
-        o: 'O',
-        p: 'P',
-        q: 'Q',
-        r: 'R',
-        s: 'S',
-        t: 'T',
-        u: 'U',
-        v: 'V',
-        w: 'W',
-        x: 'X',
-        y: 'Y',
-        z: 'Z'
-    };
+    const alphabet = {a: 'A',b: 'B',c: 'C',d: 'D',e: 'E',f: 'F',g: 'G',h: 'H',i: 'I',j: 'J',k: 'K',l: 'L',m: 'M',n: 'N',o: 'O',p: 'P',q: 'Q',r: 'R',s: 'S',t: 'T',u: 'U',v: 'V',w: 'W',x: 'X',y: 'Y',z: 'Z'};
     let newWord = str;
     let newWordFirstLetter = newWord[0];
     newWordFirstLetter = alphabet[newWordFirstLetter];
--- index.js
+++ index.js
@@ -1,40 +1,13 @@
 const unorderedList = document.getElementById('unordered-list');
 
-const alphabet = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z'
-];
+const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 let breed = [];
 
 fetch('https://dog.ceo/api/breeds/list')
     .then((res) => res.json())
     .then((res) => {
-        return (breed = res.message), populateHtml();
+        return breed = res.message, populateHtml();
     })
     .catch((err) => console.log(err));
 
@@ -85,9 +58,7 @@ const populateHtml = () => {
         if (initialLetter.nextSibling.childNodes.length === 0) {
             const noBreedFound = createHtmlElement(
                 'p',
-                `There are no breeds of dogs with the initial ${alphabet[
-                    j
-                ].toUpperCase()}`,
+                `There are no breeds of dogs with the initial ${alphabet[j].toUpperCase()}`,
                 'no-breed-found'
             );
             initialLetter.nextSibling.appendChild(noBreedFound);

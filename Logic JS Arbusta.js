76e07124743e6f50739bfc4392b9337847ef11d3
Santiago Baliño <santiagobalino@hotmail.com>
2023-04-17 11:56:50 -0300

    Small changes in Logic JS file

--- Logic JS Arbusta.js
+++ Logic JS Arbusta.js
@@ -107,6 +107,9 @@ console.log(
 
 const convertFirstLetterToUppercase = (str) => {
     const alphabet = {a: 'A',b: 'B',c: 'C',d: 'D',e: 'E',f: 'F',g: 'G',h: 'H',i: 'I',j: 'J',k: 'K',l: 'L',m: 'M',n: 'N',o: 'O',p: 'P',q: 'Q',r: 'R',s: 'S',t: 'T',u: 'U',v: 'V',w: 'W',x: 'X',y: 'Y',z: 'Z'};
+    
+    if(alphabet[str[0]] === undefined) return str
+
     let newWord = str;
     let newWordFirstLetter = newWord[0];
     newWordFirstLetter = alphabet[newWordFirstLetter];

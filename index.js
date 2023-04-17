6abb7fd903d0f0db9f57a4b9d1023057c83e2a5d
Santiago Baliño <santiagobalino@hotmail.com>
2023-04-17 11:00:53 -0300

    Refactor logic and styles from index.js

--- index.js
+++ index.js
@@ -38,30 +38,34 @@ fetch('https://dog.ceo/api/breeds/list')
     })
     .catch((err) => console.log(err));
 
+const createHtmlElement = (el, text, classname) => {
+    const newElement = document.createElement(el);
+    newElement.innerText = text;
+    newElement.className = classname;
+    return newElement;
+};
+
 // Traverse through the alphabet and create each initial list container
 const populateHtml = () => {
     for (let j = 0; j < alphabet.length; j++) {
-        const columnList = document.createElement('ul');
-        columnList.className = 'container-breed_list';
+        const columnList = createHtmlElement('ul', '', 'container-breed_list');
         unorderedList.appendChild(columnList);
 
-        const initialLetter = document.createElement('p');
-        initialLetter.innerText = `${alphabet[j].charAt(0).toUpperCase()}:`;
+        const initialLetter = createHtmlElement(
+            'p',
+            `${alphabet[j].charAt(0).toUpperCase()}:`,
+            'breed-initial'
+        );
         initialLetter.id = `${alphabet[j]}`;
-        initialLetter.className = 'breed-initial';
-
         columnList.appendChild(initialLetter);
 
-        const breedContainer = document.createElement('div');
-        breedContainer.className = 'container-list';
+        const breedContainer = createHtmlElement('div', '', 'container-list');
         columnList.appendChild(breedContainer);
 
         // Filter each breed with the current letter of the alphabet
         for (let i = 0; i < breed.length; i++) {
-            const breedListItem = document.createElement('li');
-
-            const breedLink = document.createElement('a');
-            breedLink.className = 'breed-link';
+            const breedListItem = createHtmlElement('li');
+            const breedLink = createHtmlElement('a', '', 'breed-link');
 
             if (breed[i].charAt(0) === alphabet[j]) {
                 breedLink.href = `./dogView.html?dogbreed=${breed[i]}`;
@@ -77,12 +81,14 @@ const populateHtml = () => {
 
         // Handle if there is no breed with the current letter
         if (initialLetter.nextSibling.childNodes.length === 0) {
-            const noBreedFound = document.createElement('p');
-            noBreedFound.innerText = `There are no breeds of dogs with the initial ${alphabet[
-                j
-            ].toUpperCase()}
-                 `;
-            noBreedFound.className = 'no-breed-found';
+            const noBreedFound = createHtmlElement(
+                'p',
+                `There are no breeds of dogs with the initial ${alphabet[
+                    j
+                ].toUpperCase()}
+                 `,
+                'no-breed-found'
+            );
             initialLetter.nextSibling.appendChild(noBreedFound);
         }
     }
